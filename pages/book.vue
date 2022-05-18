ea6b50ce6d6ddcfa0103d31fc6466f8a89b81f43
Mmy26 <yu.b1994masamura@gmail.com>
2022-05-18 14:02:29 +0900

    フォームの実装

--- pages/book.vue
+++ pages/book.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <NuxtChild @add-book-list="addBook" />
+    <NuxtChild :books="books" @add-book-list="addBook" />
   </div>
 </template>
 
@@ -15,14 +15,16 @@ export default {
       newBook: null,
     }
   },
-  mounted() {
+  // mountedだとeditなど子コンポーネントの処理の方が早く、間に合わないため
+  created() {
     // if文でLocalStorageが設置されているか確認
     if (localStorage.getItem(STORAGE_KEY)) {
       // 例外処理でデータがあれば追加する処理
       try {
         this.books = JSON.parse(localStorage.getItem(STORAGE_KEY))
       } catch (e) {
-        localStorage.removeItem(STORAGE_KEY)
+        // エラーが発生したらデータを削除する
+        // localStorage.removeItem(STORAGE_KEY)
       }
     }
   },
@@ -33,7 +35,7 @@ export default {
       //   return
       // }
 
-      // 配列のbooksにnewBookをPush
+      // 配列のbooksに追加する本の情報をPush
       this.books.push({
         id: this.books.length,
         title: e.title,
@@ -45,9 +47,17 @@ export default {
       // newBookを空にしておく
       // this.newBook = ''
 
-      console.log(this.books)
+      // console.log(this.books)
+
       // saveCatsメソッドを呼び出し
       this.saveBooks()
+
+      // 最後に追加したidの取得
+      // slice(-1[0]):後ろから1つ目
+      // console.log(this.books.slice(-1)[0].id)
+
+      // 画面遷移のメソッドを呼び出す
+      this.goToEditPage(this.books.slice(-1)[0].id)
     },
     removeBook(x) {
       this.books.splice(x, 1)
@@ -57,6 +67,9 @@ export default {
       const parsed = JSON.stringify(this.books)
       // LocalStorageにデータを保存
       localStorage.setItem(STORAGE_KEY, parsed)
+    },
+    goToEditPage(id) {
+      this.$router.push(`/book/edit/${id}`)
     },
   },
 }
